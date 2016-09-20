447d3a6f8006d5ac5fab2ccafb92cddb2c5bf426
maruko0713 <wanzifun@gmail.com>
2016-09-20 20:53:50 +0800

    update 09.20

--- alert/js/main.js
+++ alert/js/main.js
@@ -12,31 +12,75 @@ require(["jquery","window"],function($,w){
 		var win = new w.Window().alert({
 			title:"提示",
 			content:"welcome!",
-			handler: function(){
-				alert("you click the button");
-			},
 			width:300,
 			height:150,
 			y:50,
 			hasCloseBtn:true,
-			handler4AlertBtn:function(){
-				alert("you click the alert button");
-			},
-			handler4CloseBtn:function() {
-				alert("you click the close button");
-			},
 			text4AlertBtn:"点一点",
 			skinClassName:null,
 			hasMask:true,
 			dragHandle:".window_header"
 		}).on("alert",function(){
-			alert("the second alert handler");
-		}).on("alert",function(){
-			alert("the third alert handler");
+			alert("you click the alert button");
 		}).on("close",function(){
+			alert("you click the close button");
+		}).on("alert",function(){
+			alert("you click the second alert button");
+		});
+	});
+        $("#b").click(function(){
+		var win = new w.Window().confirm({
+			title:"提示",
+			content:"welcome!",
+			width:300,
+			height:150,
+			y:50,
+			hasCloseBtn:true,
+			text4AlertBtn:"点一点",
+			skinClassName:null,
+			hasMask:true,
+			dragHandle:".window_header"
+		}).on("confirm",function(){
+			alert("the second alert handler");
+		}).on("confirm",function(){
+			alert("the third alert handler");
+		}).on("cancel",function(){
 			alert("the second close handler");
 		});
 	});
+        $("#c").click(function(){
+		var win = new w.Window().prompt({
+			title:"提示",
+			content:"welcome!",
+			width:300,
+			height:150,
+			y:50,
+			hasCloseBtn:true,
+			text4PromptBtn:"输入",
+			skinClassName:null,
+			hasMask:true,
+			dragHandle:".window_header",
+			defaultValue4PromptInput:"请输入内容"
+		}).on("prompt",function(inputValue){
+			alert("您输入的内容是: "+inputValue);
+		}).on("cancel",function(){
+			alert("the second cancel handler");
+		});
+	});
+        $("#d").click(function(){
+		var win = new w.Window().common({
+			content:"我是一个通用弹窗",
+			width:300,
+			height:150,
+			y:50,
+			hasMask:true,
+			dragHandle:".window_header",
+			timeout:3000
+		}).on("common",function(inputValue){
+			alert("您输入的内容是: "+inputValue);
+		});
+	});
+
 });
 	
 });
--- alert/js/window.js
+++ alert/js/window.js
@@ -6,23 +6,49 @@ define(["widget","jquery","jqueryUI"],function(widget,$,$UI){
 			content:"",
 			handler: null,
 			hasCloseBtn:false,
-			handler4AlertBtn:null,
-			handler4CloseBtn:null,
 			text4AlertBtn:"确定",
+			text4CancelBtn:"取消",
+			text4ConfirmBtn:"确定",
+			text4PromptBtn:"确定",
+			isPromptInputPassword:false,
+			defaultValue4PromptInput:"",
+			maxlength4PromptInput:10,
 			hasMask:false,
 			isDraggable:true,
 			dragHandle:null,
-			handlers: {}
+			handlers: {},
+			handler4ComfirmBtn:null,
+			handler4CancelBtn:null,
+			handler4AlertBtn:null,
+			handler4CloseBtn:null,
+			handler4PromptBtn:null
 		}
 	}
 
 	Window.prototype = $.extend({},new widget.Widget(),{
 		renderUI: function() {
+			var footerContent = "";
+			switch(this.cfg.winType) {
+				case "alert":
+					footerContent = "<input type='button' value='"+this.cfg.text4AlertBtn+"'class='window_alertBtn'>";
+					break;
+				case "confirm":
+					footerContent = "<input type='button' value='" + this.cfg.text4ConfirmBtn + "' class='window_confirmBtn'><input type='button' value='"+this.cfg.text4CancelBtn+"' class='window_cancelBtn'>";
+					break;
+				case "prompt":
+					this.cfg.content += "<p class='window_promptIntputWrapper'><input type='" + (this.cfg.isPromptInputPassWord?"password":"text") + "' value='" + this.cfg.defaultValue4PromptInput + "' maxlength='" + this.cfg.maxlength4PromptInput + "' class='window_promptInput'></p>";
+					footerContent = "<input type='button' value='" + this.cfg.text4PromptBtn + "' class='window_promptBtn'><input type='button' value='" + this.cfg.text4CancelBtn + "' class='window_cancelBtn'"; 
+			        break;
+			}
 			this.boundingBox = $("<div class='window_boundingBox'>"+
-					"<div class='window_header'>" + this.cfg.title + "</div>"+
-					"<div class='window_body'>" + this.cfg.content + "</div>"+
-					"<div class='window_footer'><input class='window_alertBtn' type='button' value='"+this.cfg.text4AlertBtn+"'></div>"+
-					"</div>");
+					"<div class='window_body'>" + this.cfg.content + "</div>"+"</div>");
+					
+			if(this.cfg.winType!="common") {
+				var that = this;
+				this.boundingBox.prepend("<div class='window_header'>" + this.cfg.title + "</div>");
+				this.boundingBox.append("<div class='window_footer'>"+footerContent+"</div>");
+			}
+			this._promptInput = this.boundingBox.find(".window_promptInput");
 			if(this.cfg.hasMask){
 				this._mask = $("<div class='window_mask'></div>");
 				this._mask.appendTo("body");
@@ -38,9 +64,17 @@ define(["widget","jquery","jqueryUI"],function(widget,$,$UI){
 			this.boundingBox.on("click",".window_alertBtn",function(){
 				that.fire("alert");
 				that.destroy();
-			});
-			this.boundingBox.on("click",".window_closeBtn",function(){
+			}).on("click",".window_closeBtn",function(){
 				that.fire("close");
+				that.destroy()
+			}).on("click",".window_confirmBtn",function() {
+				that.fire("confirm");
+				that.destroy();
+			}).on("click",".window_cancelBtn",function() {
+				that.fire("cancel");
+				that.destroy();
+			}).on("click",".window_promptBtn",function(){
+				that.fire("prompt",that._promptInput.val());
 				that.destroy();
 			});
 			if(this.cfg.handler4AlertBtn) {
@@ -48,6 +82,15 @@ define(["widget","jquery","jqueryUI"],function(widget,$,$UI){
 			}
 			if(this.cfg.handler4CloseBtn) {
 				this.on("close",this.cfg.handler4CloseBtn);
+			}
+			if(this.cfg.handler4ConfirmBtn) {
+				this.on("alert",this.cfg.handler4ConfirmBtn);
+			}
+			if(this.cfg.handler4CancelBtn) {
+				this.on("close",this.cfg.handler4CancelBtn);
+			}
+			if(this.cfg.handler4PromptBtn) {
+				this.on("prompt",this.cfg.handler4PromptBtn);
 			}
 		},
 		syncUI: function() {
@@ -57,6 +100,12 @@ define(["widget","jquery","jqueryUI"],function(widget,$,$UI){
 				left:(this.cfg.x||((window.innerWidth-this.cfg.width)/2) + "px"),
 				top:(this.cfg.y || ((window.innerHeight - this.cfg.height)/2) + "px"),
 			});
+			if(this.cfg.winType=="common") {
+				var that = this;
+				setTimeout(function(){
+				    that.boundingBox.css("display","none");
+				},that.cfg.timeout);
+			}
 			if(this.cfg.skincClassName) {
 				this.boundingBox.addClass(cfg.skinClassName);
 			}
@@ -75,17 +124,28 @@ define(["widget","jquery","jqueryUI"],function(widget,$,$UI){
 		destructor:function(){
 			this._mask && this._mask.remove();
 		},
-
-		destroy: function() {
-
-		},		
 		alert: function(cfg) {
-			$.extend(this.cfg,cfg);
+			$.extend(this.cfg,cfg,{winType:"alert"});
 			this.render();
 			return this;
 		},
-		confirm:function(){},
-		prompt:function(){}
+		confirm:function(cfg) {
+			$.extend(this.cfg,cfg,{winType:"confirm"});
+			this.render();
+			return this;
+		},
+		prompt:function(cfg){
+			$.extend(this.cfg,cfg,{winType:"prompt"});
+			this.render();
+			this._promptInput.focus();
+			return this;
+		},
+		common: function(cfg) {
+			$.extend(this.cfg,cfg,{winType:"common"});
+			this.render();
+			return this;
+		}
+
 	});
 	return {
 		Window:Window
